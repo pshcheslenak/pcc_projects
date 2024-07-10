ff0903ca40f487ef6b75f21628045958907cd40c
Petr Shcheslenak <pshcheslenok@gmail.com>
2024-07-10 17:44:59 +0200

    Add dropping the fleet and changing direction

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -16,3 +16,6 @@ class Settings():
         self.bullets_allowed = 3
 
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # 1 - right; -1 to left
+        self.fleet_direction = 1
