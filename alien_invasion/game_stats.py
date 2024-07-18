a253ec72e79868907381f4a9ceb202bddba2fa09
Petr Shcheslenak <pshcheslenok@gmail.com>
2024-07-18 20:38:13 +0200

    Add high score to the game

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -8,6 +8,8 @@ class GameStats():
 
         self.game_active = False
 
+        self.high_score = 0
+
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
