eeb2a68926a9c9ce92a820eddb6ae8df2add33d3
Petr Shcheslenak <pshcheslenok@gmail.com>
2024-07-18 20:55:49 +0200

    Add level

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -76,6 +76,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             self.aliens.empty()
             self.bullets.empty()
@@ -134,6 +135,9 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
