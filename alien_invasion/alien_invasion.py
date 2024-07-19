0deeac714c9a4c146f704350802f269f31bc0b07
Petr Shcheslenak <pshcheslenok@gmail.com>
2024-07-19 08:28:51 +0200

    Add display of remaining ships

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -77,6 +77,7 @@ class AlienInvasion:
             self.stats.game_active = True
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
 
             self.aliens.empty()
             self.bullets.empty()
@@ -190,6 +191,7 @@ class AlienInvasion:
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             self.aliens.empty()
             self.bullets.empty()
