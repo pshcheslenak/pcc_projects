ce79c6c4603c0a0862e718ec83b2763b1c397bc0
Petr Shcheslenak <pshcheslenok@gmail.com>
2024-10-14 21:50:28 +0200

    Add registration page.

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -2,8 +2,13 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     # Include URL authority by default
     path('', include('django.contrib.auth.urls')),
+
+    # Registration page
+    path('register/', views.register, name='register'),
 ]
